7585417f3e540543db81eea304ae5251e4a19992
Nayem-Ul-Islam <nayemulislam606@gmail.com>
2022-02-04 23:13:18 +0600

    first commit

---
+++ lib/.env.dart
@@ -0,0 +1,1 @@
+const String googleAPIKey = 'AIzaSyB3jsXP4uP7gz6IrCqDDWmqTwg9dY6PHxE';
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/map_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: const MapScreen(),
+    );
+  }
+}
